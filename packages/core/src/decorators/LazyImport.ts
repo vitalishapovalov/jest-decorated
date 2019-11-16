70ae1d481b99c501aa5826caa74d9097f830f12b
Vitali Shapovalov <shapovalov.vitali.v@gmail.com>
2019-11-17 00:25:13 +0200

    fix typo

--- packages/core/src/decorators/LazyImport.ts
+++ packages/core/src/decorators/LazyImport.ts
@@ -8,7 +8,7 @@ export function LazyImport(path: string, getter?: ((importedModule: any) => any)
         const describeManager = DescribeManager.getDescribeManager(proto.constructor as Class);
 
         if (getter && !isCallable(getter) && !isString(getter) && !isArray(getter)) {
-            throw new SyntaxError("@LazyImport 2nd argument must be a string or a function.");
+            throw new SyntaxError("@LazyImport 2nd argument must be a string, or a function, or an array.");
         }
 
         return describeManager
