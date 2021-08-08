7131acb56f72ad3c94b86a118d2ee53ac5be7ad5
Vitali Shapovalov <shapovalov.vitali.v@gmail.com>
2021-08-08 10:12:35 +0300

    linter fixes

--- packages/core/src/decorators/Describe.ts
+++ packages/core/src/decorators/Describe.ts
@@ -34,5 +34,5 @@ function createDescribe(describeType: DescribeType): DescribeDecorator {
             describeRunner
                 .registerDescribeInJest(parentDescribeRunner);
         };
-    }
+    };
 }
